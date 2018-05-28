d6fb3d7ab0c5eb276332b9ea26b45f85678bd335
Neengash <pepmanas93@gmail.com>
2018-05-28 21:54:47 +0200

    Read the env values without carriage return

--- wendy.py
+++ wendy.py
@@ -3,6 +3,7 @@ env = {}
 def load_env_values():
   with open('.env', 'r') as f:
     for line in f:
+      line = line.split('\n')[0]
       key_value = line.split('=')
       env[key_value[0]] = key_value[1]
   return env
