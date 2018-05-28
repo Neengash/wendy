a7789f9d35fe6bedeeda823894639926a4c52357
Neengash <pepmanas93@gmail.com>
2018-05-28 21:26:12 +0200

    wendy - read env values

--- wendy.py
+++ wendy.py
@@ -1,1 +1,13 @@
+env = {}
+
+def load_env_values():
+  with open('.env', 'r') as f:
+    for line in f:
+      key_value = line.split('=')
+      env[key_value[0]] = key_value[1]
+  return env
+
 print("Hi I'm Wendy")
+
+env = load_env_values()
+
