0ad4eea20d5f5b7d876696009e4cf1662a1f07f3
Neengash <pepmanas93@gmail.com>
2018-06-04 20:18:28 +0200

    Send the email (needs localhost smtp)

--- wendy.py
+++ wendy.py
@@ -1,5 +1,8 @@
 import requests
 import json
+import smtplib
+import datetime
+from email.mime.text import MIMEText
 
 env = {}
 
@@ -13,18 +16,26 @@ def load_env_values():
 
 env = load_env_values()
 
-aemet_url = 'https://opendata.aemet.es/opendata/api/prediccion/especifica/municipio/horaria/{}'.format(env['LOCATION'])
+aemet_url = 'https://opendata.aemet.es/opendata/api/prediccion/especifica/municipio/horaria/{location}'
+location_url = aemet_url.format(location=env['LOCATION'])
 headers = {
   'cache-control': "no-cache",
   'api_key': env['API-KEY'],
 }
 
-r = requests.get(aemet_url, headers=headers).json()
+r = requests.get(location_url, headers=headers).json()
 r2 = requests.get(r['datos']).json()[0]
 
 city = r2['nombre']
 state = r2['provincia']
+
+now = datetime.datetime.now()
+
 current_prediction = r2['prediccion']['dia'][0]
+if (datetime.datetime.strptime(current_prediction['fecha'], "%Y-%m-%d").day != now.day):
+  current_prediction = r2['prediccion']['dia'][1]
+
+date = current_prediction['fecha']
 sun_up = current_prediction['orto']
 sun_down = current_prediction['ocaso']
 date = current_prediction['fecha']
@@ -36,12 +47,14 @@ for data_type in ('precipitacion', 'temperatura', 'sensTermica', 'estadoCielo'):
     value_name = 'value' if not data_type == 'estadoCielo' else 'descripcion'
     filtered_data[int(i['periodo'])][data_type]  = i[value_name]
 
-msg = """
+
+body = """
 Morning!
 
 Today, {date} sun rises at {sun_up} and sets at {sun_down}, on {city}, {state}.
 
-Detailed information:
+That's the info I could get:
+
 """.format(date=date, sun_up=sun_up, sun_down=sun_down, city=city, state=state)
 
 hour_line = "\t{time}h : {temp}º ({sens_termica}), {prob_lluvia}% lluvia y con el cielo {estado_cielo}\n"
@@ -49,7 +62,7 @@ hour_line = "\t{time}h : {temp}º ({sens_termica}), {prob_lluvia}% lluvia y con 
 for i in filtered_data:
   if len(filtered_data[i].keys()) == 0:
     continue
-  msg = msg + hour_line.format(
+  body = body + hour_line.format(
       time=i,
       temp=filtered_data[i]['temperatura'],
       sens_termica=filtered_data[i]['sensTermica'],
@@ -57,4 +70,11 @@ for i in filtered_data:
       estado_cielo=filtered_data[i]['estadoCielo']
     )
 
-print(msg)
+msg = MIMEText(body)
+msg['Subject'] = 'Wake up {name}'.format(name=env['NAME'])
+msg['From'] = env['FROM']
+msg['To'] = env['TO']
+
+s = smtplib.SMTP('localhost')
+s.send_message(msg)
+s.quit()
